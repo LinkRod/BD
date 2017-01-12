11d762a9f9c9a4ee20a61f265cd8a46b39a9fb74
LinkRod <link.hectorodr06@gmail.com>
2017-01-11 19:42:02 -0600

    Update model.rb

--- flight/app/models/model.rb
+++ flight/app/models/model.rb
@@ -1,15 +1,22 @@
 class Flight < ActiveRecord::Base
- 
+ t.string :users
+ :bookings
 end
 
-# class User 
-# end
+class User  < ActiveRecord::Base
+:fligths
+:bookings
+end
 
-# class Booking
-# end
+class Booking < ActiveRecord::Base
+:users
+:fligths
+end
 
-# class UserBooking
-# end
+class UserBooking < ActiveRecord::Base
+:userflight
+end
 
-# class UserFlight
-# end
+class UserFlight < ActiveRecord::Base
+
+end
