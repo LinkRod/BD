7c333a35fde854247a5b5131a2a25da5d8344ce6
LinkRod <link.hectorodr06@gmail.com>
2017-01-11 19:42:32 -0600

    Update seeds.rb

--- flight/db/seeds.rb
+++ flight/db/seeds.rb
@@ -1,1 +1,6 @@
 # Este archivo sirve para crear registros de prueba
+flight1 = Flight.create(num_flight:'1378', date:Time.now, depart:'2017-01-06', from:'México City', to:'Zacatecas', duration:'3:00:00', cost:'1500.00', passengers:'180')
+flight2 = Flight.create(num_flight:'5156', date:Time.now, depart:'2017-01-07', from:'México City', to:'Venecia', duration:'18:00:00', cost:'8000.00', passengers:'250')
+flight3 = Flight.create(num_flight:'4664', date:Time.now, depart:'2017-01-08', from:'México City', to:'Machu Pichu', duration:'12:00:00', cost:'4500.00', passengers:'752')
+flight4 = Flight.create(num_flight:'8456', date:Time.now, depart:'2017-01-09', from:'México City', to:'Tokio', duration:'16:00:00', cost:'7500.00', passengers:'455')
+flight5 = Flight.create(num_flight:'3455', date:Time.now, depart:'2017-01-10', from:'México City', to:'Rio de Janeiro', duration:'14:00:00', cost:'5000.00', passengers:'354')
