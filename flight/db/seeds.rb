08679ca82439307f751f53a3a85e11e4c4557a45
LinkRod <link.hectorodr06@gmail.com>
2017-01-18 14:29:40 -0600

    Update seeds.rb

--- flight/db/seeds.rb
+++ flight/db/seeds.rb
@@ -1,6 +1,10 @@
 # Este archivo sirve para crear registros de prueba
-flight1 = Flight.create(num_flight:'1378', date:Time.now, depart:'2017-01-06', from:'México City', to:'Zacatecas', duration:'3:00:00', cost:'1500.00', passengers:'180')
-flight2 = Flight.create(num_flight:'5156', date:Time.now, depart:'2017-01-07', from:'México City', to:'Venecia', duration:'18:00:00', cost:'8000.00', passengers:'250')
-flight3 = Flight.create(num_flight:'4664', date:Time.now, depart:'2017-01-08', from:'México City', to:'Machu Pichu', duration:'12:00:00', cost:'4500.00', passengers:'752')
-flight4 = Flight.create(num_flight:'8456', date:Time.now, depart:'2017-01-09', from:'México City', to:'Tokio', duration:'16:00:00', cost:'7500.00', passengers:'455')
-flight5 = Flight.create(num_flight:'3455', date:Time.now, depart:'2017-01-10', from:'México City', to:'Rio de Janeiro', duration:'14:00:00', cost:'5000.00', passengers:'354')
+flight1 = Flight.create(num_flight:'1378', date:'2017-01-06', depart:'07:15:16', from:'Mexico City', to:'Zacatecas', duration:'3:00', cost:'1500.00', passenger:'180')
+flight2 = Flight.create(num_flight:'5156', date:'2017-01-07', depart:'10:20:00', from:'Mexico City', to:'Venecia', duration:'18:00', cost:'8000.00', passenger:'250')
+flight3 = Flight.create(num_flight:'4664', date:'2017-01-08', depart:'13:15:00', from:'Mexico City', to:'Machu Pichu', duration:'12:00', cost:'4500.00', passenger:'752')
+flight4 = Flight.create(num_flight:'8456', date:'2017-01-09', depart:'08:30:16', from:'Mexico City', to:'Tokio', duration:'16:00', cost:'7500.00', passenger:'455')
+flight5 = Flight.create(num_flight:'3455', date:'2017-01-10', depart:'18:50:00', from:'Mexico City', to:'Rio de Janeiro', duration:'14:00', cost:'5000.00', passenger:'354')
+
+Admin1 = User.create(name:'Hector', email:'hector@gmail.com', admin: true)
+Admin2 = User.create(name:'Valeria', email:'valeria@gmail.com', admin: true)
+Admin3 = User.create(name:'Bolo', email:'bolo@gmail.com', admin: true)
